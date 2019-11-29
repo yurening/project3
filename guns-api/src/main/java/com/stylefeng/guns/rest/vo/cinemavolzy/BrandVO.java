11024112c0487bccd1640dd29d189655d55c4716
lzy <longzhiyyy@163.com>
2019-11-29 22:38:30 +0800

    cinema lzy

--- guns-api/src/main/java/com/stylefeng/guns/rest/vo/cinemavolzy/BrandVO.java
+++ guns-api/src/main/java/com/stylefeng/guns/rest/vo/cinemavolzy/BrandVO.java
@@ -2,8 +2,11 @@ package com.stylefeng.guns.rest.vo.cinemavolzy;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class BrandVO {
+public class BrandVO implements Serializable {
+    private static final long serialVersionUID = 8727128876262445249L;
     private Integer brandId;
     private String brandName;
     private boolean isActive;
