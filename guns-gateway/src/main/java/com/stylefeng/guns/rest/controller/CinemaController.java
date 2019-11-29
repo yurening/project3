167b4fd26f2603673db15ecbb41b890ebf6646ca
lzy <longzhiyyy@163.com>
2019-11-29 22:22:59 +0800

    cinema lzy

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/controller/CinemaController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/controller/CinemaController.java
@@ -2,10 +2,16 @@ package com.stylefeng.guns.rest.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.stylefeng.guns.rest.service.CinemaTestService;
+import com.stylefeng.guns.rest.service.cinemalzy.IMtimeCinemaTService;
 import com.stylefeng.guns.rest.vo.BaseResVO;
+import com.stylefeng.guns.rest.vo.cinemavolzy.CinemaBaseVO;
+import com.stylefeng.guns.rest.vo.cinemavolzy.CinemaConditionVO;
+import com.stylefeng.guns.rest.vo.cinemavolzy.CinemaInfoVO;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Component
 @ResponseBody
@@ -23,5 +29,18 @@ public class CinemaController {
     public BaseResVO getFields(Integer cinemaId){
         BaseResVO fields = cinemaTestService.getFieldsById(cinemaId);
         return fields;
+    }
+
+    @Reference(interfaceClass = IMtimeCinemaTService.class,check = false)
+    IMtimeCinemaTService cinemaTService;
+
+    @RequestMapping("getCinemas")
+    public CinemaBaseVO<List<CinemaInfoVO>> getCinemas(Integer brandId, Integer hallType, Integer districtId, Integer pageSize, Integer nowPage){
+        return cinemaTService.getCinemasItem(brandId,hallType,districtId,pageSize,nowPage);
+    }
+
+    @RequestMapping("getCondition")
+    public CinemaBaseVO<CinemaConditionVO> getCondition(Integer brandId, Integer hallType, Integer areaId){
+        return cinemaTService.getCinemaCondition(brandId,areaId,hallType);
     }
 }
